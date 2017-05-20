ee8a9516614f99a51239bc7b5a90f144c9c19dd7
jamesnugent00 <jnugent1311@gmail.com>
2017-05-20 13:50:53 +1000

    User authentication / needs cloudinary fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  before_action :check_if_logged_out, only: [:new, :create]
+
+  before_action :check_if_logged_in, only: [:index, :show, :edit, :update]
   # GET /users
   # GET /users.json
   def index
@@ -10,6 +13,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    id = params["id"]
+    @user = User.find_by(id: id)
   end
 
   # GET /users/new
@@ -17,58 +22,61 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
+
   def edit
+    id = params["id"]
+    @user = User.find_by(id: id)
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
 
-    respond_to do |format|
+  def create
+    @user = User.new ( user_strict_params )
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        cloudinary = Cloudinary::Uploader.upload( params["user"]["link"])
+        @user.link = cloudinary["url"]
+        @user.save
+        redirect_to user_path( @user )
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+          render :new
       end
-    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+      user = User.find_by(id: params["id"])
+      user.update( user_strict_params )
+      cloudinary = Cloudinary::Uploader.upload( params["user"]["link"])
+      user.link = cloudinary["url"]
+      user.save
+      redirect_to user_path(user)
     end
-  end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    user = User.find_by(id: params["id"])
+    user.destroy
+    redirect_to users_path()
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+    def user_strict_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :image, :team_leader)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    def check_if_logged_out
+      if @current_user
+        flash[:error] = "You are already logged in"
+        redirect_to "/users"
+      end
+    end
+
+    def check_if_logged_in
+      unless @current_user
+        flash[:error] = "You need to be logged in for that"
+        redirect_to "/login"
+      end
     end
 end
