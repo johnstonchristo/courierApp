5b325e96d0c5407b8d7c41b59bdf1d26a0ccab1c
jamesnugent00 <jnugent1311@gmail.com>
2017-05-20 15:07:29 +1000

    Associations between USER and ORDER created. Testing needed with Seeds

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+
+  belongs_to :courier, class_name: 'User', foreign_key: 'courier_id'
 end
