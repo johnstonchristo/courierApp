e44d37d2e63fadcc95287277e4decb295ce0c413
jamesnugent00 <jnugent1311@gmail.com>
2017-05-22 09:48:31 +1000

    associations work around added using .self

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,20 @@
 class Order < ApplicationRecord
+
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+#   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+#   belongs_to :courier, class_name: 'User', foreign_key: 'courier_id'
 
-  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+  def receiver
+    User.find_by id: self.receiver_id
+  end
 
-  belongs_to :courier, class_name: 'User', foreign_key: 'courier_id'
+  def courier
+    User.find_by id: self.courier_id
+  end
+
 end
+
+
+# t.integer  "sender_id"
+# t.integer  "receiver_id"
+# t.integer  "courier_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,43 @@
 User.destroy_all
 
-user_1 = User.create(first_name: "Chris", last_name: "Johnston", email: "johnstonchristo@gmail.com", password: "chicken", password_confirmation: "chicken")
+user_1 = User.create({
+  first_name: "Chris",
+  last_name: "Johnston",
+  email: "johnstonchristo@gmail.com",
+  password: "chicken",
+  password_confirmation: "chicken"
+  })
 
-user_2 = User.create(first_name: "James", last_name: "Nugent", email: "jnugent1311@gmail.com", password: "password", password_confirmation: "password")
+user_2 = User.create({
+  first_name: "James",
+  last_name: "Nugent", email: "jnugent1311@gmail.com",
+  password: "password",
+  password_confirmation: "password"
+  })
 
-user_3 = User.create(first_name: "Ja", last_name: "Rule", email: "johnstonchristo@gmail.com", password: "fyre", password_confirmation: "fyre")
+user_3 = User.create({
+  first_name: "Ja",
+  last_name: "Rule",
+  email: "johnstonchristo@gmail.com",
+  password: "fyre",
+  password_confirmation: "fyre"
+  })
 
-
+puts "User Count: #{User.all.count}"
 
 
 Order.destroy_all
 
-order_1 = Order.create(item_description: "Important files", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, state: 0)
+order_1 = Order.create({
+  item_description: "Important files",
+  item_weight: 5,
+  item_height: 10,
+  item_depth: 15,
+  sender_id: user_1.id,
+  receiver_id: user_2.id,
+  courier_id: user_3.id,
+  state: 0
+  })
 
 order_2 = Order.create(item_description: "Crack", item_weight: 3, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 1)
 
@@ -22,6 +48,8 @@ order_4 = Order.create(item_description: "Xmas present", item_weight: 9, item_he
 order_5 = Order.create(item_description: "Nosé", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 4)
 
 order_6 = Order.create(item_description: "12 bananas", item_weight: 15, item_height: 8, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 5)
+
+puts "Order Count #{Order.all.count}"
 
 
 
