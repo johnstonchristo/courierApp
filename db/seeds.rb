8fef9fd08d51a3cde8a21ca3f5fe261f53550897
johnstonchristo <johnstonchristo@gmail.com>
2017-05-22 09:59:28 +1000

    some css changes to login page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,16 @@ Order.destroy_all
 
 order_1 = Order.create(item_description: "Important files", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, state: 0)
 
-order_2 = Order.create(item_description: "Crack", item_weight: 3, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 1)
-
-order_3 = Order.create(item_description: "House keys", item_weight: 7, item_height: 4, item_depth: 17, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 2)
-
-order_4 = Order.create(item_description: "Xmas present", item_weight: 9, item_height: 3, item_depth: 11, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 3)
-
-order_5 = Order.create(item_description: "Nosé", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 4)
-
-order_6 = Order.create(item_description: "12 bananas", item_weight: 15, item_height: 8, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 5)
-
+# order_2 = Order.create(item_description: "Crack", item_weight: 3, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 1)
+#
+# order_3 = Order.create(item_description: "House keys", item_weight: 7, item_height: 4, item_depth: 17, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 2)
+#
+# order_4 = Order.create(item_description: "Xmas present", item_weight: 9, item_height: 3, item_depth: 11, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 3)
+#
+# order_5 = Order.create(item_description: "Nosé", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 4)
+#
+# order_6 = Order.create(item_description: "12 bananas", item_weight: 15, item_height: 8, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 5)
+#
 
 
 # 0 - Created (sender_id has been assigned)
