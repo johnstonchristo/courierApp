5dffe837652dd22904b905023a543222c54e9f81
johnstonchristo <johnstonchristo@gmail.com>
2017-05-22 17:08:41 +1000

    added extra seed data + got pending_orders page working

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,4 +50,26 @@ order_5 = Order.create(item_description: "Nosé", item_weight: 5, item_height: 1
 
 order_6 = Order.create(item_description: "12 bananas", item_weight: 15, item_height: 8, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, courier_id: user_3.id, state: 5)
 
+order_7 = Order.create({
+  item_description: "Important files",
+  item_weight: 5,
+  item_height: 10,
+  item_depth: 15,
+  sender_id: user_1.id,
+  state: 0,
+  })
+
+order_8 = Order.create(item_description: "Rocks", item_weight: 3, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 0)
+
+order_9 = Order.create(item_description: "Octopus", item_weight: 7, item_height: 4, item_depth: 17, sender_id: user_1.id, receiver_id: user_2.id, state: 0)
+
+order_10 = Order.create(item_description: "Indoor Plants", item_weight: 9, item_height: 3, item_depth: 11, sender_id: user_1.id, receiver_id: user_2.id, state: 0)
+
+order_11 = Order.create(item_description: "Nose beers", item_weight: 5, item_height: 10, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 0)
+
+order_12 = Order.create(item_description: "Chairs x 4", item_weight: 15, item_height: 8, item_depth: 15, sender_id: user_1.id, receiver_id: user_2.id, state: 0)
+
+
+
+
 puts "Order Count #{Order.all.count}"
