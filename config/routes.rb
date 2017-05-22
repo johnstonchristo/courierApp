6f12611f1a61959f51c508ea7e6d0580cf9d916e
jamesnugent00 <jnugent1311@gmail.com>
2017-05-22 16:34:47 +1000

    logout added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
-  delete '/logout' => 'session#destroy'
+  get '/logout' => 'session#destroy'
 
   get '/orders/pending_orders' => 'orders#show_pending_orders'
 
