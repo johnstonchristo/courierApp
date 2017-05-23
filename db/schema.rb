5140240be984f7bb7f2f09cc813fd093f68d02c2
jamesnugent00 <jnugent1311@gmail.com>
2017-05-23 15:07:35 +1000

    first google map plug working - hard coded

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520032657) do
+ActiveRecord::Schema.define(version: 20170523010912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "type"
+    t.text     "name"
+    t.text     "address"
+    t.text     "latitude"
+    t.text     "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string   "item_description"
