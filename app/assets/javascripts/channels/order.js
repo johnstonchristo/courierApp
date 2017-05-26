a9d91bc8652c97f6f20aac684d397d304bafb7bc
jamesnugent00 <jnugent1311@gmail.com>
2017-05-26 10:13:50 +1000

    change to courier marker

--- app/assets/javascripts/channels/order.js
+++ app/assets/javascripts/channels/order.js
@@ -34,8 +34,8 @@ App.order = App.cable.subscriptions.create({
                 lat: latitude,
                 lng: longitude
               },
-              map: map,
-              icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+              map: map
+              // icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
             });
       }
     }
