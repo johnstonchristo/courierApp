1f6dbc9acfd74d7166b4239eb4d3178ab66f3f68
jamesnugent00 <jnugent1311@gmail.com>
2017-05-26 10:58:14 +1000

    updated courier marker to original

--- app/assets/javascripts/channels/order.js
+++ app/assets/javascripts/channels/order.js
@@ -39,7 +39,7 @@ App.order = App.cable.subscriptions.create({
                 lng: longitude
               },
               map: map,
-              icon: 'https://d30y9cdsu7xlg0.cloudfront.net/png/536-200.png'
+              icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png'
             });
         map.setCenter({
           lat: latitude,
